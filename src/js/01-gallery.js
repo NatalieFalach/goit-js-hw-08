4e596bb4c57c012d7160b46bae9dd38a4b765140
NatalieFalach <natkacandy@gmail.com>
2022-10-28 14:29:16 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href='${original}'>
+            <img class="gallery__image" src='${preview}' alt='${description}' />
+        </a>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
