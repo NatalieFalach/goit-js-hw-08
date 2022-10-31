376eeb1e55e9de4c241dcbc3fd441d79c8e629c4
NatalieFalach <natkacandy@gmail.com>
2022-10-31 21:06:46 +0200

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const localStorageKeyName = 'videoplayer-current-time';
+const currentTime = window.localStorage.getItem(localStorageKeyName);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function onPlayerTimeUpdate(e) {
+  window.localStorage.setItem(localStorageKeyName, e.seconds);
+}
+
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackStateKey = 'feedback-form-state';
+let feedbackState = localStorage.getItem(feedbackStateKey);
+
+if (feedbackState) {
+  const data = JSON.parse(feedbackState);
+  const { email, message } = feedbackForm.elements;
+
+  email.value = data.email;
+  message.value = data.message;
+}
+
+feedbackForm.addEventListener('input', throttle(onFormChange, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(JSON.parse(feedbackState));
+
+  localStorage.removeItem(feedbackStateKey);
+  feedbackForm.reset();
+  feedbackState = null;
+}
+
+function onFormChange(e) {
+  const { email, message } = feedbackForm.elements;
+  const feedBackObject = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(feedbackStateKey, JSON.stringify(feedBackObject));
+}
